9aed30476e3f60d868b158e14ad4a991f2b28764
sudoswift <sudoswift@gmail.com>
2021-05-26 21:06:34 +0900

    Initial Commit

---
+++ Toast_Popup_tutorial/Toast_Popup_tutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  Toast_Popup_tutorialApp.swift
+//  Toast_Popup_tutorial
+//
+//  Created by izvern on 2021/05/26.
+//
+
+import SwiftUI
+
+@main
+struct Toast_Popup_tutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
